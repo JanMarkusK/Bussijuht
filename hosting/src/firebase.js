27aed9e030947fddfca7cf522acb89365c2077d5
Hans Oskar Trolla <146342767+hansotTLU@users.noreply.github.com>
2024-06-01 16:22:11 +0300

    õige firebase.js

--- hosting/src/firebase.js
+++ hosting/src/firebase.js
@@ -1,20 +1,23 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com", // Ensure this is the correct URL
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBfYoZeDgpcTAtja_GdeM13JtYkmWZIKUc",
+  authDomain: "bussijuht.firebaseapp.com",
+  projectId: "bussijuht",
+  storageBucket: "bussijuht.appspot.com",
+  messagingSenderId: "774160422123",
+  appId: "1:774160422123:web:b6f7995f5d6dba33c65aae",
+  measurementId: "G-73LLQLQZ0M"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const realtimeDB = getDatabase(app);
+const analytics = getAnalytics(app);
 
 export { realtimeDB };
