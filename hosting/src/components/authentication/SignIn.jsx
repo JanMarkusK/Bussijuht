d2fa8c079a61ed5ac95b99bc3029f558f013ab9a
Ken Rasmus Kuning <kunn@tlu.ee>
2024-06-11 19:35:00 +0300

    uuem

--- hosting/src/components/authentication/SignIn.jsx
+++ hosting/src/components/authentication/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebase";
+import { auth } from "../../firebase.js";
 
 
 const SignIn = () => {
