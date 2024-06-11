cd58564b5c2de438b31166b49f638212da2f6d3e
Ken Rasmus Kuning <146323669+kunn28@users.noreply.github.com>
2024-06-11 16:08:23 +0300

    11.06

--- hosting/src/components/HomePageÕige.jsx
+++ hosting/src/components/HomePageÕige.jsx
@@ -6,23 +6,34 @@ import '../assets/css/HomePage.css';
 import '../assets/css/banner.css';
 import '../assets/css/bannerbuss.css';
 import drinkingImage from '../assets/drinking.png'; 
+import bannerImage from '/banner/bannerRinde.png';
+import porgandImage from '/banner/porgand.png';
+import rahaImage from '/banner/200.png';
+import viinImage from '/banner/viin.png';
 import teeImage from '/banner/tee.png';
 import bussImage from '/banner/buss.png';
 import peatusImage from '/banner/peatus.png';
 import smokeImage from '/banner/smoke.png';
-import pealkiriImage from '/banner/pealkiri.png';
 
 const HomePageBanner = () => {
   return (
+    <>
+      <div className="banner">
+        <img src={bannerImage} alt="Banner" className="banner-image" />
+        <img src={porgandImage} alt="Porgand" className="overlay-image porgand" />
+        <img src={rahaImage} alt="200" className="overlay-image raha" />
+        <img src={viinImage} alt="Viin" className="overlay-image viin" />
+      </div>
+
       <div className="bannerbussijuht">
         <div className="road-container">
-          <img id="tee" src={teeImage} alt="tee" className="tee-image" />
+          <img src={teeImage} alt="tee" className="tee-image" />
           <img id="buss" src={bussImage} alt="buss" className="overlay-image buss" />
           <img id="peatus" src={peatusImage} alt="peatus" className="overlay-image peatus" />
           <img id="smoke" src={smokeImage} alt="smoke" className="overlay-image smoke" />
-          <img id="pealkiri" src={pealkiriImage} alt="pealkiri" className="overlay-image pealkiri" />
         </div>
       </div>
+    </>
   );
 };
 
@@ -49,42 +60,55 @@ const HomePage = ({ setInLobby, setInRules, setInCreateAccount, setInLogIn }) =>
     <div className="home-page">
       <header id="pagebegin">
         <img 
+          src={bannerImage} 
+          alt="lehe bänner" 
+          className="banner"
+        />
+        <div id="peamine" className="peamine">
+          <div className="peamine-border">
+            <img 
+              className="porgandilaadnetoode" 
+              src={porgandImage} 
+              alt="porgand" 
+            />
+          </div>  
+          <img 
+            className="raha" 
+            src={rahaImage} 
+            alt="klots" 
+          />
+          <img 
+            className="viin" 
+            src={viinImage} 
+            alt="alx" 
+          />
+        </div>
+        <img 
           src={teeImage} 
           alt="lehe bänner" 
           className="bannerbussijuht"
         />
-        <div class="animation-container">
-          <div class="box"></div>
-          <div class="invisible-box box1"></div>
-          <div class="invisible-box box2"></div>
-          <div id="alumine" className="alumine">
-            <div className="alumine-border">
-              <img 
-                id="buss" 
-                className="buss" 
-                src={bussImage} 
-                alt="buss" 
-              />
-            </div>  
+        <div id="alumine" className="alumine">
+          <div className="alumine-border">
             <img 
-              id="peatus" 
-              className="peatus" 
-              src={peatusImage} 
-              alt="peatus" 
+              id="buss" 
+              className="buss" 
+              src={bussImage} 
+              alt="buss" 
             />
-            <img 
-              id="smoke" 
-              className="smoke" 
-              src={smokeImage} 
-              alt="smoke" 
-            />
-            <img 
-              id="pealkiri" 
-              className="pealkiri" 
-              src={pealkiriImage} 
-              alt="pealkiri" 
-            />
-          </div>
+          </div>  
+          <img 
+            id="peatus" 
+            className="peatus" 
+            src={peatusImage} 
+            alt="peatus" 
+          />
+          <img 
+            id="smoke" 
+            className="smoke" 
+            src={smokeImage} 
+            alt="smoke" 
+          />
         </div>
       </header>
       <main>
