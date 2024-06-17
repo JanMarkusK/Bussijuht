ed1e9e1865e839b1e4877d3e89f4a3de52a89e4b
Joosep Madar <146342736+joosep5@users.noreply.github.com>
2024-06-17 18:19:43 +0300

    kaardi taguse jaoks vajalik

--- hosting/src/components/Card.jsx
+++ hosting/src/components/Card.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 const Card = ({ card, onClick, cardBack }) => {
   const cardFileName = card.faceUp ? `${card.value.replace(/ /g, '_')}.png` : cardBack;
   const cardImage = `../../cards/${cardFileName}`;
+  const cardBack = `../../cards/back${cardFileName}`;
+  // Wherever you're rendering the Card component
+//<Card card={yourCard} onClick={yourOnClickFunction} cardBack={selectedCardBack} />
+
 
   return (
     <div className={`card ${card.faceUp ? 'flipped' : ''}`} onClick={onClick}>
